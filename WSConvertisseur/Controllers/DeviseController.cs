12ee51560a9d2bd3a7de6cef192eabecf6602648
Jeremy Merle <jeremy.merle@protonmail.com>
2018-09-27 17:37:50 +0200

    initial commit

---
+++ WSConvertisseur/Controllers/DeviseController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WSConvertisseur.Models;
+
+namespace WSConvertisseur.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Devise")]
+    public class DeviseController : Controller
+    {
+
+        private List<Devise> _devises;
+
+        public DeviseController()
+        {
+            _devises = new List<Devise>();
+            _devises.Add(new Devise(1, "Dollar", 1.08));
+            _devises.Add(new Devise(2, "Franc Suisse", 1.07));
+            _devises.Add(new Devise(3, "Yen", 120));
+        }
+
+        // GET: api/Devise
+        [HttpGet]
+        public IEnumerable<Devise> GetAll()
+        {
+            return _devises;
+        }
+
+        // GET: api/Devise/5
+        [HttpGet("{id}", Name = "GetDevise")]
+        public IActionResult Get(int id)
+        {
+            Devise devise = _devises.FirstOrDefault((d) => d.Id == id);
+            if(devise == null)
+            {
+                return NotFound();
+            }
+            return Ok(devise);
+        }
+        
+        // POST: api/Devise
+        [HttpPost]
+        public IActionResult Post([FromBody]Devise devise)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _devises.Add(devise);
+            return CreatedAtRoute("GetDevise", new { id = devise.Id }, devise);
+        }
+        
+        // PUT: api/Devise/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Devise devise = _devises.FirstOrDefault((d) => d.Id == id);
+            if (devise == null)
+            {
+                return NotFound();
+            }
+            _devises.Remove(devise);
+            return Ok(devise);
+          
+        }
+    }
+}
---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string NomDevise { get; set; }
+
+        public double Taux { get; set; }
+
+        public Devise()
+        {
+
+        }
+
+        public Devise(int Id, string NomDevise, double Taux)
+        {
+            this.Id = Id;
+            this.NomDevise = NomDevise;
+            this.Taux = Taux;
+
+        }
+    }
+}
